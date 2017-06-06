f39a73fbbd922841cd2e0756a043fec8717868a0
davros1973 <gitHub@xarta.co.uk>
2017-06-06 20:29:48 +0100

    Local gulp and gulp tasks working

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// Sass configuration
+// https://code.visualstudio.com/docs/languages/css
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(function(f) {
+            return f.base;
+        }))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('css/*.scss', ['sass']);
+})
