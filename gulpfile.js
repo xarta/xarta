ae4692baea0254360e0eaa133e42e798e613a123
davros1973 <gitHub@xarta.co.uk>
2017-06-09 09:47:27 +0100

    Lots of tweaks, added package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // Sass configuration
 // https://code.visualstudio.com/docs/languages/css
-var gulp = require('gulp');
+// https://www.npmjs.com/package/gulp-minify
+var gulp = require('gulp');             // look for gulp in package node-modules
 var sass = require('gulp-sass');
+var minify = require('gulp-minify');
 
 gulp.task('sass', function() {
     gulp.src('css/*.scss')
@@ -14,3 +16,16 @@ gulp.task('sass', function() {
 gulp.task('default', ['sass'], function() {
     gulp.watch('css/*.scss', ['sass']);
 })
+
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
