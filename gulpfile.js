8f676d4116bf2590ae4e463995a9801afea4c14f
davros1973 <gitHub@xarta.co.uk>
2017-06-10 15:07:53 +0100

    tweaked menu, and now minify html in workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,24 @@
 // https://www.npmjs.com/package/gulp-minify
 // https://www.npmjs.com/package/gulp-concat
 
+// https://www.npmjs.com/package/gulp-htmlmin
+// https://github.com/kangax/html-minifier
+
+// https://www.npmjs.com/package/gulp-rename
+// https://www.npmjs.com/package/gulp-replace
+
 // https://github.com/OverZealous/run-sequence   (I want to concat, then compress)
 //                                               e.g. ['task1', 'task2'], 'task3', ['t4', 't5']
 //                                                      parallel       then series then parallel
+
+/* NOTE: Windows paths ... I'm using mapped UNC path as L:\
+         ... however glob pattern matching doesn't work 
+         ... properly with this ... e.g. if cwd is L:\
+         ... then wildcard matching won't work for files
+         ... in cwd.  Even if L:/ is specified.
+         
+         ... Have to use a subdirectory.
+*/
 
 var gulp = require('gulp');             // look for gulp in package node-modules
 var sass = require('gulp-sass');
@@ -15,6 +30,19 @@ var minify = require('gulp-minify');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+
+gulp.task('minify-html', function() {
+    return gulp.src("html-debug/*-debug.html")
+        .pipe (htmlmin({collapseWhitespace: true, conservativeCollapse: true, caseSensitive: true, minifyJS: true, removeComments: true, removeEmptyElements: false }))
+        .pipe (replace('<script></script>', ''))
+        .pipe (rename(function (path) {
+            path.basename = path.basename.replace("-debug", "");
+        }))
+        .pipe(gulp.dest('./'))
+});
 
 gulp.task('sass', function() {
     gulp.src('css/css-debug/*.scss')
@@ -68,6 +96,7 @@ gulp.task('concat-minify-home-js', function() {
 gulp.task('xarta', function() {
     gulp.watch('css/css-debug/*.scss', ['sass']);
     gulp.watch('css/css-debug/*.css', ['minify-css']);
+    gulp.watch('html-debug/*.html', ['minify-html']);
     gulp.watch('js-debug-home/*.js', ['concat-minify-home-js']);
 });
 
